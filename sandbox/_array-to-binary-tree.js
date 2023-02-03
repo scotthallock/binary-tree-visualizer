6d3c8313ac6fe662a8ee3d26949bf92f3e715beb
Scott Hallock <scott.hallock@gmail.com>
2023-02-03 12:48:05 -0600

    add todo list (todo.md), add sandbox algorithm for converting array to binary tree

---
+++ sandbox/_array-to-binary-tree.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    constructor (val, left, right) {
+        this.val = (val===undefined ? 0 : val);
+        this.left = (left===undefined ? null : left);
+        this.right = (right===undefined ? null : right);
+    }
+}
+
+/**
+ * @param {Array} arr 
+ * @returns {TreeNode}
+ */
+const buildTreeFromArray = (arr) => {
+    const root = new TreeNode(arr.shift());
+    const queue = [root];
+    while(queue.length > 0 && arr.length > 0) {
+        const currNode = queue.shift();
+        const leftVal = arr.shift();
+        const rightVal = arr.shift();
+
+        // support both null elements and empty (undefined) elements
+        if (leftVal !== null && leftVal !== undefined) {
+            currNode.left = new TreeNode(leftVal);
+            queue.push(currNode.left);
+        }
+        if (rightVal !== null && leftVal !== undefined) {
+            currNode.right = new TreeNode(rightVal);
+            queue.push(currNode.right);
+        }
+    }
+    return root;
+};
+
+
+console.log(buildTreeFromArray([1, 2, null, 3, 4, 5, 6]));
---
+++ todo.md
@@ -0,0 +1,18 @@
+# Todo List
+
+1. Generating binary tree from array representation (DONE)
+2. Generating DOM elements (SVG elements) from binary tree 
+    - Drawings nodes
+    - Drawing edges between nodes
+    - Formatting position of nodes for normal tree view
+    - Formatting position of nodes for compact tree view
+3. Create new nodes from mouse clicks
+    - Hover-over area under node to see a faint node
+    - Click to add the node to the DOM and the tree
+4. Click a node to edit its value
+5. Right click a node to remove it (and its children)
+6. Add form at top of page:
+    - input array representation
+    - generate button
+    - have animation/focus to show when the field changes
+    - handle invalid inputs
